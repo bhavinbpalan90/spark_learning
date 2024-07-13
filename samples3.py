c033ee22a70edb492ab208cd8411803347486ae4
sfc-gh-bhavinpalan <bhavin.palan@snowflake.com>
2024-07-13 15:21:50 -0400

    Read from Public S3

--- samples3.py
+++ samples3.py
@@ -1,11 +1,16 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
+import time
  
 conf = SparkConf()
 conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
 conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
+#conf.set('spark.hadoop.fs.s3a.access.key', '')
+#conf.set('spark.hadoop.fs.s3a.secret.key', '')
+#conf.set('spark.hadoop.fs.s3a.session.token', <token>) 
  
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
- 
-df = spark.read.csv('s3a://noaa-ghcn-pds/csv.gz/1929.csv.gz', inferSchema=True)
+
+#df = spark.read.parquet(f's3a://bp-parquet/DATAEXPORT/CRUNCHBASE_2.parquet_0_0_0.snappy.parquet', inferSchema=True) 
+df = spark.read.parquet(f's3a://bp-parquet/DATAEXPORT/CRUNCHBASE2/CRUNCHBASE_2.parquet_0_0_0.snappy.parquet', inferSchema=True)
 df.show()
